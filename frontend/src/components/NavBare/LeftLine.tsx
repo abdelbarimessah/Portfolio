7ac9e9436259b689560df46e1f7e5b3bb07a9f70
abdelbari <abdelbari@abdelbari.abdelabri>
2024-06-14 13:10:39 +0100

    ADD: the Project part and the rectangle in both side and also the alignement of the page

--- frontend/src/components/NavBare/LeftLine.tsx
+++ frontend/src/components/NavBare/LeftLine.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function LeftLine() {
   return (
-    <div className=" ml-[17px] w-[32px] h-[311px] flex justify-start items-center flex-col gap-2">
+    <div className="fixed left-0 top-0 ml-[17px] w-[32px] h-[311px] flex justify-start items-center flex-col gap-2">
       <div className="w-[1px] h-[191px] bg-color-3"></div>
       <div className="flex flex-col items-center justify-center z-50">
         <div className="relative w-[32px] h-[32px] flex items-center hover:scale-90  cursor-pointer justify-center ">
@@ -16,7 +16,7 @@ export default function LeftLine() {
         <div className="relative w-[32px] h-[32px] flex items-center justify-center cursor-pointer hover:scale-90 ">
           <Image src="./assets/linkedin.svg" fill={true} alt=""></Image>
         </div>
-        <div className="relative w-[32px] h-[32px] flex items-center justify-center hover:scale-90 cursor-pointer" >
+        <div className="relative w-[32px] h-[32px] flex items-center justify-center hover:scale-90 cursor-pointer">
           <Image src="./assets/mail.svg" fill={true} alt=""></Image>
         </div>
       </div>
