469a90b36e4552f416775b861509a24cfff078f1
abdelbari <abdelbari@abdelbari.abdelabri>
2024-07-03 21:20:12 +0100

    FIX: the color issue in the home page and the body

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -20,12 +20,15 @@ export default function RootLayout({
   return (
     <html lang="en" className="font-fira-code">
       <link rel="icon" href="/assets/favicon.ico" sizes="any" />
-      <body className="no-scrollbar overflow-x-hidden bg-color-1 flex flex-col w-screen min-h-screen relative">
+      <body className="no-scrollbar overflow-x-hidden  flex flex-col w-screen min-h-screen relative">
+        <div className="flex flex-col flex-1 bg-color-1">
+
         <div className="flex items-center justify-center w-full absolute top-0 ">
           <LeftLine />
           <NavBare />
         </div>
         {children}
+        </div>
       </body>
     </html>
   );
